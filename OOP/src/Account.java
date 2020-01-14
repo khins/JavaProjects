2bb5d5ad4d9702edd183c526cebec0151a18927a
kevin <kevin@hinshawfam.com>
2020-01-14 05:30:40 -0600

    Inputting and outputting floating-point numbers with Account objects

--- OOP/src/Account.java
+++ OOP/src/Account.java
@@ -1,10 +1,25 @@
 // Fig. 3.1 Account.java
 // Account class that contains a name instance variable and methods to set and get its value
+// adds Account class with a double instance variable balance and a constructor and deposit method that perform validation
 public class Account {
 	private String name; 
+	private Double balance;
 	
-	public Account(String name) {
+	public Account(String name, Double balance) {
 		this.name = name;
+		if (balance > 0.0)
+			this.balance = balance;
+		else
+			this.balance = 0.0;
+	}
+	
+	public void deposit(Double depoistAmount) {
+		if (depoistAmount > 0.0)
+			this.balance = this.balance + depoistAmount;
+	}
+	
+	public Double getBalance() {
+		return this.balance;
 	}
 	
 	public void setName(String name) {
--- OOP/src/AccountTest.java
+++ OOP/src/AccountTest.java
@@ -6,14 +6,30 @@ public class AccountTest {
 	public static void main(String[] args) {
 
 		//	Using the Account constructor to initialize the name instance variable when each Account object is created
-		Account account1 = new Account("John Doe");
-		Account account2 = new Account("Jane Dough");
+		Account account1 = new Account("John Doe", 50.00);
+		Account account2 = new Account("Jane Dough", -7.53);
 		
-		System.out.printf("Account1 is: %s%n", account1.getName());
-		System.out.printf("Account2 is: %s%n", account2.getName()); 
+		System.out.printf("%s balance: $%.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n", account1.getName(), account2.getBalance());
 		
+		Scanner input = new Scanner(System.in);
 		
-
+		System.out.print("Enter deposit amount for account1: ");
+		double depoistAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n", depoistAmount);
+		account1.deposit(depoistAmount);
+		
+		//display balances
+		System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
+		
+		System.out.print("Enter deposit amount for account2: ");
+		depoistAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance%n%n", depoistAmount);
+		account2.deposit(depoistAmount);
+		
+		System.out.printf("%s balance: %.2f%n", account1.getName(), account1.getBalance());
+		System.out.printf("%s balance: %.2f%n", account2.getName(), account2.getBalance());
 	}
 
 }
